f591b2e5735bfbaaa90b779e0a461db01b15dffb
jaswanthsurya <jassusai0611@gmail.com>
2019-02-03 11:37:51 +0530

    Added Rotate irhgt program

---
+++ c/cassignments/cassignments/Rotate_Right(12)/Rotate_Right.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void rotate_right(long int, int);
+int main()
+{
+	long int n = 0;
+	int b = 0;
+	printf("enter the number  :");
+	scanf("%ld", &n);
+	printf("enter numbers of bits to be shifted :");
+	scanf("%d",&b);
+	rotate_right(n, b);
+	return 0;
+}
+void rotate_right(long int n, int b)
+{
+	long int res = 0;
+	res = n >> b;
+	printf("%ld rotated right by %d bits gives: %ld",n,b,res);
+}
