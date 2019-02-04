f62e576207777f345097cf6dd5f997a1db858ba9
jaswanthsurya <jassusai0611@gmail.com>
2019-02-04 21:33:42 +0530

    Added factorial program

---
+++ c/cassignments/Factorial/Factorial.c
@@ -0,0 +1,24 @@
+//program to find factorial of given number
+
+#include<stdio.h>
+long int fact(int);
+int main()
+{
+	long int num=0,res=0;
+	printf("enter the number : ");
+	scanf_s("%ld", &num);
+	res = fact(num);
+	printf("factorial of %ld is : %ld", num, res);
+}
+
+long int fact(int num)
+{
+	if (num == 0 || num == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return num * fact(num - 1);
+	}
+}
