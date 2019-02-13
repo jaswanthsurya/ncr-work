f07dd9b6fa1893257211b642e83438b490ecbbc9
jaswanthsurya <jassusai0611@gmail.com>
2019-02-13 15:21:43 +0530

    added swapby value and swap by reference program

---
+++ cppassignments/swap/swapbyuser.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+void swapbyval(int, int);
+void swapbyref(int *, int *);
+int main()
+{
+	int n = 0,a=0,b=0;
+	cout << "enter input to proceed" << endl;
+	cout << "1.swap by value" << endl;
+	cout << "2.swap by reference" << endl;
+	cin >> n;
+	cout << "enter the values" << endl;
+	cin >> a;
+	cin >> b;
+	switch (n)
+	{
+	case 1:swapbyval(a,b);
+		break;
+	case 2:swapbyref(&a,&b);
+		break;
+	default: cout << "invalid" << endl;
+	}
+	cout<<"result after swapping" << endl;
+	cout << a << " " << b << endl;
+	return 0;
+}
+void swapbyval(int a, int b)
+{
+	int temp = 0;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void swapbyref(int *a, int *b)
+{
+	int temp = 0;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
