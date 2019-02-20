3054b080188590e350e10be34b18aed9594c35b7
jaswanthsurya <jassusai0611@gmail.com>
2019-02-20 14:07:40 +0530

    insertion sort program

---
+++ ds/dsassignments/InsertionSort/InsertionSort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;;
+
+int main()
+{
+	int n = 0, i = 0, j = 0, temp = 0;
+	int *a;
+	cout << "enter size of array : ";
+	cin >> n;
+	a = new int[n];
+	cout << "enter array elements: ";
+	for (i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	for (i = 0; i < n - 1; i++)
+	{
+		j = i;
+		temp = a[j + 1];
+		while (temp < a[j] && j>=0)
+		{
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = temp;
+	}
+	cout << "the sorted array is: ";
+	for (i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+}
