139d292dccfe47cb801a9910eb8471600923d713
jaswanthsurya <jassusai0611@gmail.com>
2019-02-27 10:52:04 +0530

    command line api in windows to get command line arguments

--- windowsInternals/CommandLineApi/CommandLineApi.cpp
+++ windowsInternals/CommandLineApi/CommandLineApi.cpp
@@ -7,10 +7,12 @@ int main()
 {
 	LPWSTR *args;
 	int argSize;
-	args = CommandLineToArgvW(GetCommandLine(), &argSize);
+	args = CommandLineToArgvW(GetCommandLine()//returnrs the address of the command string 
+		, &argSize//return the size of the total number of arguments it is a output parameter for the function 
+	);//function to retrieve the command line arguments without using arguments for the main() the return type is a long pointer to wide string
 	for (int i = 0; i < argSize; i++)
 	{
-		wcout << "[" << i << "]: " << args[i] << endl;
+		wcout << "[" << i << "]: " << args[i] << endl;//wcout to print the widestring characters
 	}
 	return 0;
 }
---
+++ windowsInternals/CommandLineApiUsingTchar/CommandLineArgUsingTchar.cpp
@@ -0,0 +1,17 @@
+#include<Windows.h>
+#include<iostream>
+#include <tchar.h>
+
+using namespace std;
+
+int _tmain(int argc//argument count
+	, TCHAR argv[],//agrument list
+	TCHAR env[]//windows provides an additional parameter for the tmain() function which is the environmental variables 
+)
+{
+	for (int counter = 0; counter < argc; counter++)
+	{
+		cout << argv[counter] << endl;
+	}
+	return 0;
+}
