cbb66ea7ee32839e3f7ed455784d8e1658be6a70
jaswanthsurya <jassusai0611@gmail.com>
2019-02-27 16:27:09 +0530

    command line arguments from main using _tmain()

--- windowsInternals/CommandLineApiUsingTchar/CommandLineArgUsingTchar.cpp
+++ windowsInternals/CommandLineApiUsingTchar/CommandLineArgUsingTchar.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 
 int _tmain(int argc//argument count
-	, TCHAR argv[],//agrument list
-	TCHAR env[]//windows provides an additional parameter for the _tmain() function which is the environmental variables 
+	, WCHAR * argv[],//agrument list
+	WCHAR * env[]//windows provides an additional parameter for the _tmain() function which is the environmental variables 
 )
 {
 	for (int counter = 0; counter < argc; counter++)
 	{
-		cout << argv[counter] << endl;
+		wcout << argv[counter] << endl;
 	}
 	return 0;
 }
