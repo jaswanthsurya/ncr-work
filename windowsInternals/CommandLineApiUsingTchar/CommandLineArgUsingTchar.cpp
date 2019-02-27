2d9cddf0147a6fc3a038f4cc9ffc509b67c94752
jaswanthsurya <jassusai0611@gmail.com>
2019-02-27 10:54:59 +0530

    command line apis in windows to get command line arguments

--- windowsInternals/CommandLineApiUsingTchar/CommandLineArgUsingTchar.cpp
+++ windowsInternals/CommandLineApiUsingTchar/CommandLineArgUsingTchar.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int _tmain(int argc//argument count
 	, TCHAR argv[],//agrument list
-	TCHAR env[]//windows provides an additional parameter for the tmain() function which is the environmental variables 
+	TCHAR env[]//windows provides an additional parameter for the _tmain() function which is the environmental variables 
 )
 {
 	for (int counter = 0; counter < argc; counter++)
